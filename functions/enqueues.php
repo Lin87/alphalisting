f2f9c27c7f97909c04db503211d926e8d49a820f
Ethan Lin <ethan.lin.05@gmail.com>
2026-04-09 10:53:53 -0400

    refactor: remove unused tabs integration code

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -12,10 +12,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Register default A-Z stylesheet, jQuery-UI Tabs script and add our enqueue
+ * Register default A-Z stylesheet and add our enqueue
  * functions to the `wp_enqueue_scripts` action
  *
- * @since 2.0.0 Renamed from alphalisting_add_styling. Added jQuery-UI Tabs support.
+ * @since 2.0.0 Renamed from alphalisting_add_styling.
  * @return void
  */
 function alphalisting_do_enqueue() {
@@ -33,17 +33,9 @@ function alphalisting_do_enqueue() {
 		ALPHALISTING_VERSION
 	);
 
-	wp_register_script(
-		'alphalisting-tabs',
-		plugins_url( 'scripts/alphalisting-tabs.js', dirname( __FILE__ ) ),
-		array( 'jquery', 'jquery-ui-tabs' ),
-		ALPHALISTING_VERSION,
-		true
-	);
-
-	wp_register_script(
-		'alphalisting-widget-admin',
-		plugins_url( 'scripts/alphalisting-widget-admin.js', dirname( __FILE__ ) ),
+    wp_register_script(
+        'alphalisting-widget-admin',
+        plugins_url( 'scripts/alphalisting-widget-admin.js', dirname( __FILE__ ) ),
 		array( 'jquery', 'jquery-ui-autocomplete' ),
 		ALPHALISTING_VERSION,
 		true
@@ -80,27 +72,5 @@ function alphalisting_do_enqueue() {
 
 	add_action( 'customize_controls_enqueue_scripts', 'alphalisting_customize_enqueue_styles' );
 
-	$tabify = get_option( 'alphalisting-add-tabs', false );
-	/**
-	 * Determine whether to add jQuery-UI Tabs
-	 *
-	 * @param bool True to add jQuery-UI Tabs, False to disable.
-	 * @since 2.0.0
-	 */
-	$tabify = apply_filters( 'alphalisting_tabify', $tabify );
-	/**
-	 * Determine whether to add jQuery-UI Tabs
-	 *
-	 * @param bool True to add jQuery-UI Tabs, False to disable.
-	 * @since 2.0.0
-	 */
-	$tabify = apply_filters( 'alphalisting-tabify', $tabify ); //phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
-
-	if ( defined( 'ALPHALISTING_LOG' ) && ALPHALISTING_LOG ) {
-		do_action( 'alphalisting_log', 'AlphaListing: Tabify', $tabify );
-	}
-	if ( true === $tabify && ! has_action( 'wp_enqueue_scripts', 'alphalisting_enqueue_tabs' ) ) {
-		add_action( 'wp_enqueue_scripts', 'alphalisting_enqueue_tabs' );
-	}
 }
 add_action( 'init', 'alphalisting_do_enqueue' );
--- functions/scripts.php
+++ functions/scripts.php
@@ -12,43 +12,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Enqueue Tabs script on pages where the shortcode is active
- *
- * @since 2.0.0
- * @param bool $force Set this to true if you want the script to always be enqueued.
- * @return void
- */
-function alphalisting_enqueue_tabs( bool $force = false ) {
-	global $post;
-	if ( $force || ( is_singular() && has_shortcode( $post->post_content, 'alphalisting' ) ) ) {
-		wp_enqueue_script( 'alphalisting-tabs' );
-	}
-}
-
-/**
- * Forcibly enqueue Tabs script. This is a helper function which can be hooked in-place of the default hook added in `alphalisting_add_styling`
- *
- * @since 2.0.0
- * @return void
- */
-function alphalisting_force_enqueue_tabs() {
-	alphalisting_enqueue_tabs( true );
-}
-
-/**
- * Replace the default Tabs script enqueue function with `alphalisting_force_enqueue_tabs` to always add the Tabs script to pages
- *
- * @since 2.0.0
- * @return void
- */
-function alphalisting_force_enable_tabs() {
-	if ( false !== has_action( 'wp_enqueue_scripts', 'alphalisting_enqueue_tabs' ) ) {
-		remove_action( 'wp_enqueue_scripts', 'alphalisting_enqueue_tabs' );
-	}
-	add_action( 'wp_enqueue_scripts', 'alphalisting_force_enqueue_tabs' );
-}
-
-/**
  * Enqueue the widget configuration support script
  *
  * @since 2.1.0
